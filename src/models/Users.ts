84948e769168e6a2145b552a50dec36e3116439f
João Neves <joao.neves@enecad.com.br>
2020-07-07 22:42:30 -0300

    created users model and its migration

--- src/models/Users.ts
+++ src/models/Users.ts
@@ -6,13 +6,19 @@ import {
     UpdateDateColumn,
 } from 'typeorm';
 
-@Entity('appointments')
-class Appointment {
+@Entity('users')
+class Users {
     @PrimaryGeneratedColumn('uuid')
     id: string;
 
     @Column()
-    provider: string;
+    name: string;
+
+    @Column()
+    email: string;
+
+    @Column()
+    password: string;
 
     @Column('time with time zone')
     date: Date;
@@ -24,4 +30,4 @@ class Appointment {
     updated_at: Date;
 }
 
-export default Appointment;
+export default Users;
